6479d24229d56650143c85ef462976b769458e19
coldtang <ttang6471@gmail.com>
2019-12-25 16:52:40 +0800

    添加业务层文件

--- OnePiece.Business/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OnePiece.Business
-{
-    public class Class1
-    {
-    }
-}
---
+++ OnePiece.Business/RecursiveBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnePiece.Business
+{
+    /// <summary>
+    /// 递归函数方法论（时间复杂度和空间复杂度的初步探讨）
+    /// </summary>
+    public class RecursiveBusiness
+    {
+
+    }
+}
