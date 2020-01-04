560ffc3b60afd1d9f07a1bbe99cbc0c6c6a62df2
唐善群 <tsq49510@ly.com>
2020-01-04 23:00:20 +0800

    SingleExample

---
+++ OnePiece.Business/Common/Something.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnePiece.Business.Common
+{
+    /// <summary>
+    /// 单实例
+    /// </summary>
+    public sealed class SingleExample
+    {
+        public SingleExample()
+        {         
+        }
+
+        public static SingleExample Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        // 使用内部类+静态构造函数实现延迟初始化
+        class Nested
+        {
+            static Nested() { }
+
+            internal static readonly SingleExample instance = new SingleExample();
+        }
+    }
+}
